321342a259a03b2f697775e454c2eb14dccc2095
samsalvatico <samuele.salvatico@gmail.com>
2022-02-06 16:15:36 +0100

    Start process tour validation

--- app/Http/Controllers/TourOperatorsController.php
+++ app/Http/Controllers/TourOperatorsController.php
@@ -2,22 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ProcessTourRequest;
 use Illuminate\Http\JsonResponse;
 
 class TourOperatorsController extends Controller
 {
-    public function processTour(Request $request): JsonResponse
-    {
-        return new JsonResponse();
-    }
-
-    public function addTour(Request $request): JsonResponse
-    {
-        return new JsonResponse();
-    }
-
-    public function removeTour(Request $request): JsonResponse
+    public function importTours(ProcessTourRequest $request): JsonResponse
     {
         return new JsonResponse();
     }
