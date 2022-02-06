7e56abd0bcd0f625ee4d315afecaa3bf3fdd16bb
samsalvatico <samuele.salvatico@gmail.com>
2022-02-06 15:31:22 +0100

    Added tests configuration and fixed phpcs issues

---
+++ app/Http/Controllers/TourOperatorsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class TourOperatorsController extends Controller
+{
+    public function processTour(Request $request): JsonResponse
+    {
+        return new JsonResponse();
+    }
+
+    public function addTour(Request $request): JsonResponse
+    {
+        return new JsonResponse();
+    }
+
+    public function removeTour(Request $request): JsonResponse
+    {
+        return new JsonResponse();
+    }
+}
